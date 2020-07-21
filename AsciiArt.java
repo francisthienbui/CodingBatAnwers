7ac276a1636ee3a06305b1358b9402a59b313a35
Francis <francisthienbui@gmail.com>
2020-07-21 11:02:37 -0600

    AsciiArt Project

---
+++ AsciiArt.java
@@ -0,0 +1,24 @@
+/**
+ * Lab 1 ASCII Art. Created on 1/24/2020. Due by 1/31/2020 at 11:59 PM
+ * @author Francis Bui
+ */
+
+
+public class AsciiArt {
+
+    /**
+     * Prints my name in ASCII Art to the console.
+     * @param args Command-line arguments are ignored.
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println("hellow world");
+        System.out.println("  ______                                 _       ");
+        System.out.println(" |  ____|                               (_)");
+        System.out.println(" | |__     _ __    __ _   _ __     ___   _   ___ ");
+        System.out.println(" |  __|   | '__|  / _` | | '_ \\   / __| | | / __|");
+        System.out.println(" | |      | |    | (_| | | | | | | (__  | | \\__ \\");
+        System.out.println(" |_|      |_|     \\__,_| |_| |_|  \\___| |_| |___/");
+    }
+}
